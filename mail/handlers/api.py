6c1280ac1e3ba419b9855e3dcc3562f5177e03fa
saymonp <saymon.t@outlook.com>
2021-05-24 21:36:06 -0300

    Remove try

--- mail/handlers/api.py
+++ mail/handlers/api.py
@@ -5,21 +5,18 @@ from mail.mail import Mail
 
 
 @lambda_method
-def send_email(event, context):
-    try:
-        body = json.loads(event["body"])
+def send_contact_email(event, context):
+    body = json.loads(event["body"])
 
-        first_name = body["firstName"]
-        last_name = body["lastName"]
-        client_email = body["clientEmail"]
-        subject = body["subject"]
-        message = body["message"]
+    first_name = body["firstName"]
+    last_name = body["lastName"]
+    client_email = body["clientEmail"]
+    subject = body["subject"]
+    message = body["message"]
 
-        reply_to = f"{first_name} {last_name} <{client_email}>"
+    reply_to = f"{first_name} {last_name} <{client_email}>"
 
-        email = Mail()
-        email.send_email(reply_to, subject, message)
+    email = Mail()
+    email.send_email(reply_to, subject, message)
 
-        return {"ok": "Email sent"}
-    except Exception as e:
-        raise AppError(f"Email sending failed {e}")
+    return {"ok": "Email sent"}
